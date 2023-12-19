17ed2e95f60e646df0e9825641ae6a92ba08ef64
0hscar <oscar.weber00@gmail.com>
2023-12-19 22:56:17 +0200

    first commit

---
+++ SecretSanta.py
@@ -0,0 +1,22 @@
+import random
+
+def secret_santa(names):
+    # Shuffling the list of names
+    shuffled_names = names[:]
+    random.shuffle(shuffled_names)
+
+    # Assigning Secret Santa pairs in a circular fashion
+    pairs = {}
+    for i in range(len(shuffled_names)):
+        if i == len(shuffled_names) - 1:
+            pairs[shuffled_names[i]] = shuffled_names[0]
+        else:
+            pairs[shuffled_names[i]] = shuffled_names[i + 1]
+
+    return pairs
+
+# Example usage
+participants = ["Make", "Kaitsu", "Mazzeri", "Nikler", "Emil", "Dänk"]
+pairs = secret_santa(participants)
+for giver, receiver in pairs.items():
+    print(f"{giver} -> {receiver}")
